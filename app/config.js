4aa8e7c13fd7fe282fd3b14c934b79e22bbca341
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-05 15:34:08 -0800

    changed to new mongolab db

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ var Promise = require('bluebird');
 var mongoose = Promise.promisifyAll(require('mongoose'));
 
 // if(process.env.NODE_ENV === 'production') {
-  mongoose.connect('mongodb://MongoLab-l:zeJPQzbSoEPpww0FLheAvTPTffOL5aU1625E5bFeq1g-@ds052408.mongolab.com:52408/MongoLab-l');
+  mongoose.connect('mongodb://bjarke:bjarke@ds052408.mongolab.com:52408/shrty');
 // } else {
 //   mongoose.connect('mongodb://localhost:27017/shrty');
 // }
