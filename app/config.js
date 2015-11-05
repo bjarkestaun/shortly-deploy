9380047a8fc46149a5a5a3c7503d7e82f6ea30bc
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-05 15:27:16 -0800

    changed so db only runs on mongolab also on local

--- app/config.js
+++ app/config.js
@@ -1,10 +1,10 @@
 var Promise = require('bluebird');
 var mongoose = Promise.promisifyAll(require('mongoose'));
 
-if(process.env.NODE_ENV === 'production') {
+// if(process.env.NODE_ENV === 'production') {
   mongoose.connect('mongodb://MongoLab-l:zeJPQzbSoEPpww0FLheAvTPTffOL5aU1625E5bFeq1g-@ds052408.mongolab.com:52408/MongoLab-l');
-} else {
-  mongoose.connect('mongodb://localhost:27017/shrty');
-}
+// } else {
+//   mongoose.connect('mongodb://localhost:27017/shrty');
+// }
 
 module.exports = mongoose;
